a8c225d9e81718cf6cf84c076997f8deac0b6950
Azat <azat4dev@gmail.com>
2023-06-05 00:49:23 +0400

    Add test_map__success for CurrenciesEndpointDataMapperTests

--- CurrencyConverterTests/Data/API/Endpoints/Currencies/CurrenciesEndpointDataMapperTests.swift
+++ CurrencyConverterTests/Data/API/Endpoints/Currencies/CurrenciesEndpointDataMapperTests.swift
@@ -19,6 +19,8 @@ final class CurrenciesEndpointDataMapperTests: XCTestCase {
         return CurrenciesEndpointDataMapperImpl()
     }
     
+    // MARK: - Methods
+    
     func test_map__invalid_data() async throws {
         
         // Given
@@ -28,11 +30,43 @@ final class CurrenciesEndpointDataMapperTests: XCTestCase {
         // When
         let result = sut.map(invalidJSONData)
         
+        // Then
+        // Must fail
         guard case .success = result else {
             return
         }
         
-        // Must fail
         XCTFail("Must fail")
+    }
+    
+    func test_map__success() async throws {
+        
+        // Given
+        let sut = createSUT()
+        
+        let validJSONData = """
+            {
+                "AED": "United Arab Emirates Dirham",
+                "AFN": "Afghan Afghani",
+                "ALL": "Albanian Lek",
+                "AMD": "Armenian Dram",
+                "ANG": "Netherlands Antillean Guilder",
+                "AOA": "Angolan Kwanza",
+                "ARS": "Argentine Peso"
+            }
+        """.data(using: .utf8)!
+        
+        // When
+        let result = sut.map(validJSONData)
+        
+        // Then
+
+        guard case .success(let currencies) = result else {
+            XCTFail("Must succeed")
+            return
+        }
+        
+        XCTAssertEqual(currencies.count, 7)
+        XCTAssertEqual(currencies["AED"], "United Arab Emirates Dirham")
     }
 }
