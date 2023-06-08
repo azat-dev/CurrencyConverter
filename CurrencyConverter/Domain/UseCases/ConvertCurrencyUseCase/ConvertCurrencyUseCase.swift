a2419ef60f2f4bf282164e96531324f899f6096f
Azat <azat4dev@gmail.com>
2023-06-08 04:19:50 +0400

    Add ConvertCurrencyUseCase

---
+++ CurrencyConverter/Domain/UseCases/ConvertCurrencyUseCase/ConvertCurrencyUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  ConvertCurrencyUseCase.swift
+//  CurrencyConverter
+//
+//  Created by Azat Kaiumov on 08.06.23.
+//
+
+import Foundation
+
+// MARK: - Interfaces
+
+public enum ConvertCurrencyUseCaseError: Error {
+    
+    case internalError
+}
+
+public protocol ConvertCurrencyUseCase {
+    
+    func convert(
+        amount: Double,
+        from: CurrencyCode
+    ) async -> Result<[CurrencyCode: Double], ConvertCurrencyUseCaseError>
+}
+
